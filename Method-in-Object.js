bf2e762d15892657b9cf081593b01b9c0afaba55
Aliyan Chanegaon <111250756+AliyanChanegaon@users.noreply.github.com>
2023-03-17 13:44:04 +0000

    Methods in Object.

---
+++ Method-in-Object.js
@@ -0,0 +1,15 @@
+// passing parameters and printing values.
+let obj = {
+  print: function(a, b) {
+    console.log(a);
+    console.log(b);
+  }
+};
+// declaring variable and passing in object's function
+
+let x = "Aliyan";
+let y = "Ali";
+obj.print(x, y);
+
+
+
--- Objects-with-for-in-loop.js
+++ Objects-with-for-in-loop.js
@@ -11,6 +11,8 @@ Object = {
 };
 
 
+
+
 // for in loop (declare a variable for eg i in below case and 
 // Write in after declaration and write the name of array or  object through which we want to tranverse!!!!)
 for( let i in Object){
