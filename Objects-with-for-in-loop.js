e9dc7c4c18ba3623205ca12088b6f72d1151b50a
Aliyan Chanegaon <111250756+AliyanChanegaon@users.noreply.github.com>
2023-03-17 05:58:09 +0000

    Objects with for in loop

---
+++ Objects-with-for-in-loop.js
@@ -0,0 +1,20 @@
+// Objects with forloop
+
+
+Object = {
+
+  name: "Aliyan",
+  surname: "Chanegaon",
+  DOB:"02/12/2000",
+  Gender:"Male",
+  Location:"Nashik"
+};
+
+
+// for in loop (declare a variable for eg i in below case and 
+// Write in after declaration and write the name of array or  object through which we want to tranverse!!!!)
+for( let i in Object){
+
+  console.log(Object[i]);
+}
+
